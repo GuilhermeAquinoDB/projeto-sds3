0a9ed772f23e645a77083bc7ec7ea4081aa715b2
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-05-05 19:15:08 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -48,6 +48,41 @@ const DataTable = () => {
                         <td>21</td>
                         <td>19015.00</td>
                     </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>20/11/2020</td>
+                        <td>Douglas Luiz</td>
+                        <td>25</td>
+                        <td>12</td>
+                        <td>14000.00</td>
+                    </tr>
+                    <tr>
+                        <td>29/01/2019</td>
+                        <td>Cristina Column</td>
+                        <td>42</td>
+                        <td>11</td>
+                        <td>18520.00</td>
+                    </tr>
+                    <tr>
+                        <td>05/12/2013</td>
+                        <td>Alicia Gonçalves</td>
+                        <td>27</td>
+                        <td>10</td>
+                        <td>21250.00</td>
+                    </tr>
+                    <tr>
+                        <td>15/10/2015</td>
+                        <td>Dominic Toreto</td>
+                        <td>39</td>
+                        <td>21</td>
+                        <td>19015.00</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
